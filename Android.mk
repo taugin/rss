820ebc0a7aa9fdd2c357b8ffffc9508ca6991e27
taugin <taugin@163.com>
2013-08-06 13:21:55 +0800

    update to com.android.rss

--- Android.mk
+++ Android.mk
@@ -1,13 +1,8 @@
-# ============================================================
-# This is generated based on build.xml. Please don't edit it.
-# Author: Frank Liu <prnq63@motorola.com>
-# ============================================================
 LOCAL_PATH:= $(call my-dir)
 
-#############hdpi
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := rss 
+LOCAL_PACKAGE_NAME := rss
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := eng
 
--- src/com/android/rss/util/Log.java
+++ src/com/android/rss/util/Log.java
@@ -1,8 +1,8 @@
 package com.android.rss.util;
 
 public class Log {
-    private static final boolean DEBUG = false;
-    private static final boolean ERROR = false;
+    private static final boolean DEBUG = true;
+    private static final boolean ERROR = true;
     public static final void d(String tag, String msg){
         if(DEBUG){
             android.util.Log.d(tag, msg);
