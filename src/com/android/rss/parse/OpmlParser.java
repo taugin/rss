1e61a95bb9951dc7bb89ed7a9a983cc143447242
liuzhao.wei <liuzhao.wei@chukong-inc.com>
2014-12-25 19:45:17 +0800

    1. Optimize Log.java , format OpmlParse.java modified:   res/values/dimens.xml modified:   src/com/android/rss/parse/OpmlParser.java modified:   src/com/android/rss/util/Log.java

--- src/com/android/rss/parse/OpmlParser.java
+++ src/com/android/rss/parse/OpmlParser.java
@@ -26,63 +26,70 @@ public class OpmlParser extends Thread {
 
     private Context mContext;
     private String mOpmlUrl;
-    public OpmlParser(Context context, String opmlUrl){
+
+    public OpmlParser(Context context, String opmlUrl) {
         mContext = context;
         mOpmlUrl = opmlUrl;
     }
+
     @Override
     public void run() {
         parse();
     }
-    
-    private void parse(){
-        try{
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
-            DocumentBuilder docBuilder = factory.newDocumentBuilder();  
-            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("sina_news_opml.xml");
+
+    private void parse() {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder docBuilder = factory.newDocumentBuilder();
+            InputStream is = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream("sina_news_opml.xml");
             is = NetworkUtil.getRSSInputStream(mOpmlUrl);
-            Document doc = docBuilder.parse(new InputSource(is));//http://rss.sina.com.cn/sina_news_opml.xml  
-            //root
-            Element root = (Element) doc.getDocumentElement();  
-            System.out.println("rootName : " + t.getNodeName());  
-            NodeList nodeList = doc.getElementsByTagName("outline");  
-            Element outlineElement = null;  
-            
+            Document doc = docBuilder.parse(new InputSource(is));// http://rss.sina.com.cn/sina_news_opml.xml
+            // root
+            Element root = (Element) doc.getDocumentElement();
+            System.out.println("rootName : " + root.getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("outline");
+            Element outlineElement = null;
+
             ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
             Builder builder = null;
             ContentProviderOperation cpo = null;
             ContentValues values = null;
             String text = null;
             String xmlUrl = null;
-            for (int i = 0; i < nodeList.getLength(); i++) {  
-                if (nodeList.item(i) instanceof Element) {  
-                    outlineElement = (Element) nodeList.item(i);  
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                if (nodeList.item(i) instanceof Element) {
+                    outlineElement = (Element) nodeList.item(i);
                     text = outlineElement.getAttribute("text");
                     xmlUrl = outlineElement.getAttribute("xmlUrl");
-                    if(!TextUtils.isEmpty(xmlUrl)){
+                    if (!TextUtils.isEmpty(xmlUrl)) {
                         values = new ContentValues();
                         values.put(Constant.Content.HISTORY_TITLE, text);
                         values.put(Constant.Content.HISTORY_URL, xmlUrl);
-                        values.put(Constant.Content.FEED_GUID, String.valueOf(xmlUrl.hashCode()));
-                        builder = ContentProviderOperation.newInsert(Constant.Content.HISTORY_URI);
+                        values.put(Constant.Content.FEED_GUID,
+                                String.valueOf(xmlUrl.hashCode()));
+                        builder = ContentProviderOperation
+                                .newInsert(Constant.Content.HISTORY_URI);
                         builder = builder.withValues(values);
                         cpo = builder.build();
                         ops.add(cpo);
                     }
-                }  
-            }  
-            
+                }
+            }
+
             try {
-                mContext.getContentResolver().applyBatch(Constant.Content.AUTHORITY, ops);
-                
+                mContext.getContentResolver().applyBatch(
+                        Constant.Content.AUTHORITY, ops);
+
             } catch (RemoteException e) {
                 e.printStackTrace();
             } catch (OperationApplicationException e) {
                 e.printStackTrace();
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            
+
         }
     }
 }
